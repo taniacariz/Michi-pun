b47fd64592f89a4272eab5020af6425910a87e0e
tanimikyu <bloergesh@gmail.com>
2022-01-11 10:03:10 -0300

    general changes, store calculator, close buttom

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-//form login const
-
+// form login const
 const user = document.getElementById("userLogin");
 const pass = document.getElementById("passwordLogin");
+
+if (localStorage.getItem("result")) {
+  nextPage();
+}
 
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,15 +16,15 @@ formLogin.addEventListener("submit", (e) => {
   } else if (pass.value.length < 6) {
     alert("La contraseña tener al menos 6 caracteres");
   } else if (user.value !== "omicron") {
-    alert("El usuario ingresado no es correcto");
+    alert("Los datos son incorrectos");
   } else if (pass.value !== "omicron") {
-    alert("La contraseña ingresada no es correcta");
+    alert("Los datos son incorrectos");
   } else if (user.value === "omicron" && pass.value === "omicron") {
     nextPage();
   }
 });
 
-//change page options
+// change page options
 
 function nextPage() {
   document.getElementById("login-container").style.display = "none";
@@ -31,83 +34,146 @@ function nextPage() {
 //array options machine
 
 const listChoices = ["piedra", "papel", "tijera"];
-
-//variables
+const results = {
+  WIN: "Ganaste",
+  LOOSE: "Perdiste",
+  TIE: "Empate",
+};
 
 const stone = "piedra";
 const paper = "papel";
 const scissors = "tijera";
 
-const stoneUser = document.getElementById("piedraUser");
-const paperUser = document.getElementById("papelUser");
-const scissorsUser = document.getElementById("tijeraUser");
+const WIN = "WIN";
+const LOOSE = "LOOSE";
+const TIE = "TIE";
 
-const win = 0;
-const lose = 1;
-const tie = 2;
+const INITIAL_SCORE = 0;
+const WIN_VALUE = 100;
+const LOOSE_VALUE = 30;
+const TIE_VALUE = 0;
+const MAX_ROUNDS = 10;
+let actualScore = 0;
+
+const userImg = document.getElementById("userImg");
+const machineImg = document.getElementById("machineImg");
+const optionButtons = document.getElementsByClassName("game-option");
+
+// print values or set if does not exist
+if (!localStorage.getItem("result")) {
+  localStorage.setItem("result", "[]");
+} else {
+  printResultList();
+  checkIfOver();
+}
+
+function changeImageOption(element, option) {
+  element.src = `images/${option}.svg`;
+}
 
 //clicks btns
-
-stoneUser.addEventListener("click", () => {
-  startGame(stone);
-  console.log(stone);
-});
-
-paperUser.addEventListener("click", () => {
-  startGame(paper);
-  console.log(paper);
-});
-
-scissorsUser.addEventListener("click", () => {
-  startGame(scissors);
-  console.log(scissors);
+Array.from(optionButtons).forEach((o) => {
+  o.addEventListener("click", (e) => {
+    const option = e.currentTarget.getAttribute("data-option");
+    startGame(option);
+    changeImageOption(userImg, option);
+  });
 });
 
 function startGame(userChoise) {
   const machineChoise = Math.floor(Math.random() * listChoices.length);
   const resultMachine = listChoices[machineChoise];
-  console.log(resultMachine);
+  console.log("Option machine is " + resultMachine);
 
-  const result = resultChoise(userChoise, resultMachine);
-  switch (result) {
-    case win:
-      document.getElementById("result").innerHTML = "Ganaste";
-      break;
-    case lose:
-      document.getElementById("result").innerHTML = "Perdiste";
-      break;
-    case tie:
-      document.getElementById("result").innerHTML = "Empate";
+  changeImageOption(machineImg, resultMachine);
+
+  const result = resultChoice(userChoise, resultMachine);
+
+  document.getElementById("resultText").innerHTML = results[result];
+  document.getElementById("score").innerHTML = counterScore(result);
+
+  addToLocalStorage(userChoise, resultMachine, result);
+  printResultList();
+  checkIfOver();
+}
+
+function checkIfOver() {
+  // disable buttons when rounds are over
+  // o = option
+  if (!isPlaying()) {
+    Array.from(optionButtons).forEach((o) => {
+      o.setAttribute("disabled", true);
+    });
   }
 }
 
-function resultChoise(userChoise, resultMachine) {
+function resultChoice(userChoise, resultMachine) {
+  if (!userChoise) {
+    console.error("No hay selección");
+    return;
+  }
   if (userChoise === resultMachine) {
     console.log("Empate");
-    return tie;
+    return TIE;
   }
-  if (userChoise === stone && resultMachine === paper) {
+  if (
+    (userChoise === stone && resultMachine === paper) ||
+    (userChoise === paper && resultMachine === scissors) ||
+    (userChoise === scissors && resultMachine === stone)
+  ) {
     console.log("Perdiste");
-    return lose;
+    return LOOSE;
   }
-  if (userChoise === stone && resultMachine === scissors) {
+  if (
+    (userChoise === stone && resultMachine === scissors) ||
+    (userChoise === paper && resultMachine === stone) ||
+    (userChoise === scissors && resultMachine === paper)
+  ) {
     console.log("Ganaste");
-    return win;
-  }
-  if (userChoise === paper && resultMachine === scissors) {
-    console.log("Perdiste");
-    return lose;
-  }
-  if (userChoise === paper && resultMachine === stone) {
-    console.log("Ganaste");
-    return win;
-  }
-  if (userChoise === scissors && resultMachine === paper) {
-    console.log("Ganaste");
-    return win;
-  }
-  if (userChoise === scissors && resultMachine === stone) {
-    console.log("Perdiste");
-    return lose;
+    return WIN;
   }
 }
+
+function isPlaying() {
+  const rounds = JSON.parse(localStorage.getItem("result")).length;
+  return rounds < MAX_ROUNDS;
+}
+
+function counterScore(result) {
+  if (result === WIN) {
+    actualScore += WIN_VALUE;
+  } else if (result === LOOSE) {
+    actualScore -= LOOSE_VALUE;
+  }
+  return actualScore;
+}
+
+function addToLocalStorage(userOption, machineOption, result) {
+  const savedResults = JSON.parse(localStorage.getItem("result"));
+  savedResults.push({ userOption, machineOption, result, actualScore });
+  localStorage.setItem("result", JSON.stringify(savedResults));
+}
+
+function printResultList() {
+  const listScore = document.getElementById("list-score");
+  listScore.innerHTML = "";
+  const result = JSON.parse(localStorage.getItem("result"));
+  result.forEach((e) => {
+    const newLi = document.createElement("li");
+    newLi.appendChild(
+      document.createTextNode(
+        `${e.userOption} - ${e.machineOption} - ${e.result} - ${e.actualScore}`
+      )
+    );
+    listScore.appendChild(newLi);
+  });
+}
+
+function closeSession() {
+  localStorage.clear();
+  window.location.href = "index.html";
+}
+
+document
+  .getElementById("close-session")
+  .addEventListener("click", closeSession);
